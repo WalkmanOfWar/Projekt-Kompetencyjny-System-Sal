8f175e04d4eccdf71dff6c9b97b86e217820d7e7
PiotrPabich <87279370+PiotrPabichCode@users.noreply.github.com>
2023-04-03 19:09:40 +0200

    Changes to main page

--- react-frontend/src/components/Footer.js
+++ react-frontend/src/components/Footer.js
@@ -1,39 +1,41 @@
-import React from "react"
-import "./Footer.css"
+import React from "react";
+import "./Footer.css";
 
 export default function Footer() {
-    return (
-        <div className="container-fluid footer-background footer-text p-5">
-            <div className="row pt-5">
-                <div className="col-12">
-                    <div className="d-flex flex-row justify-content-center">
-                        <div className="col-2">
-                            <a className="nav-link" href="http://localhost:3000/">Strona Główna</a>
-                        </div>
-                        <div className="col-2">
-                            <a className="nav-link" href="http://localhost:3000/">FAQ</a>
-                        </div>
-                        <div className="col-2">
-                            <a className="nav-link" href="http://localhost:3000/">O nas</a>
-                        </div>
-                        <div className="col-2">
-                            <a className="nav-link" href="http://localhost:3000/">Kontakt</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="m-5 ">
-                <h1 className="text-center">@2023 Time Tacticians</h1>
-                <h2 className="text-center">Made by:</h2>
-                <h2 className="text-center"> 
-                    <ul>
-                        <li>Piotr Pabich</li>
-                        <li>Maciej Sierzputowski</li>
-                        <li>Szymon Owczarek</li>
-                        <li>Wiktor Koprowski</li>
-                    </ul>
-                </h2>
-            </div>                    
+  return (
+    <div className="container-fluid footer-background">
+      <footer className="py-1">
+        <ul className="nav justify-content-center border-bottom py-3 mb-3">
+          <li className="nav-item">
+            <a className="nav-link link-primary px-3 text-white" href="#">
+              Strona główna
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link link-primary px-3 text-white" href="#">
+              FAQ
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link link-primary px-3 text-white" href="#">
+              O nas
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link link-primary px-3 text-white" href="#">
+              Kontakt
+            </a>
+          </li>
+        </ul>
+        <div className="text-center text-white">
+          <p>Made by:</p>
+          <p>Piotr Pabich</p>
+          <p>Maciej Sierzputowski</p>
+          <p>Szymon Owczarek</p>
+          <p>Wiktor Koprowski</p>
+          <p>@2023 Time Tacticians</p>
         </div>
-    )
+      </footer>
+    </div>
+  );
 }
