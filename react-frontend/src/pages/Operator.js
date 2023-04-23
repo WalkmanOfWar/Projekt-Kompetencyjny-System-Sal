39d5a1603d810d5d61f19427ff50fa22d39a3324
dan525555 <77828669+dan525555@users.noreply.github.com>
2023-04-24 00:17:03 +0200

    Add operator

---
+++ react-frontend/src/pages/Operator.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import './Profile.css';
+// import '../components/ProfileInfo'
+// import '../components/ProfileReservations'
+import ProfileInfo from "../components/ProfileInfo";
+import ProfileReservations from "../components/ProfileReservations";
+function Profile  () {
+
+    const [activeComponent, setActiveComponent] = useState('ProfileInfo');
+
+  const handleClick = (component) => {
+    setActiveComponent(component);
+  };
+
+    return(
+    <div className="profile-container">
+        <div className="profile-wrapper">
+            <div className="profile-content-wrapper">
+                <div className="avatar">
+                    <div className="img"/>
+                </div>
+                <div className="column-wrapper">
+                    <span className="text header">Szymon</span>
+                    <span className="text">Moje konto / Profil</span>
+                </div>
+            </div>
+            <div className="profile-content-wrapper">
+                <div className="column-wrapper center">
+                    <div className="avatar" >
+                        <div className="img"/>
+                    </div>
+                    {/* <span className="text">Informacje o koncie</span> */}
+                    <span className="text" onClick={() => handleClick('ProfileInfo')}>Profil</span>
+                    <span className="text" onClick={() => handleClick('ProfileReservations')}>Moje rezerwacje</span>
+
+        
+                    <span className="text">Wyloguj się</span>
+                    
+                </div>
+                <div className="divider" />
+                
+                {activeComponent === 'ProfileInfo' ? <ProfileInfo /> : <ProfileReservations />}
+
+                {/* <ProfileInfo/> */}
+            </div>
+        </div>
+    </div>
+    )
+}
+
+export default Profile;
